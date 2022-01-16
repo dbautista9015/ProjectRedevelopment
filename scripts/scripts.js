8e53f27912849a2ff20de542784a8bc1e7d9f25b
Daniel Bautista <dbautista9015@gmail.com>
2022-01-15 17:25:30 -0800

    Change lat and long

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,10 @@
 function initMap() {
     var options = {
         center: {
-            lat: 38.3460,
-            lng: -0.4907
+            lat: 38.132153,
+            lng: -121.273985
         },
-        zoom: 8
+        zoom: 19
     }
 
     map = new google.maps.Map(document.getElementById("map"),options);
